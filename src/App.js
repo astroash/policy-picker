b18635129765007ff07645061188ac670e385688
Morkeltry <morkeltry@mailfence.com>
2017-10-04 12:49:23 +0100

    #28 added router, Route to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
 import './css/App.css';
 import PolicyCarousel from './components/PolicyCarousel';
 import Home from './components/Home';
+
 class App extends Component {
   constructor() {
     super();
@@ -16,10 +18,15 @@ class App extends Component {
 
   render() {
     return (
-      <main className="App mw6 tc center">
-        <h1>Policy Picker</h1>
-        <Home />
-      </main>
+      <Switch>
+        <Route exact path='/' render={() => <Home />} />
+        <Route exact path='/ownership' render={() => <PolicyCarousel />} />
+        <Route exact path='/tax' render={() => <PolicyCarousel />} />
+        <Route exact path='/technology' render={() => <PolicyCarousel />} />
+        <Route exact path='/welfare' render={() => <PolicyCarousel />} />
+        <Route exact path='/workplace' render={() => <PolicyCarousel />} />
+        {/* <Route exact path='/askanon' render={() => <Dummy />} /> */}
+      </Switch>
     );
   }
 }
