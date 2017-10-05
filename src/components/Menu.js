8c56fe52f96e572d5020c0123e4898c9e0232fc9
ameliejyc <amelie.chan@gmail.com>
2017-10-05 23:05:04 +0100

    #33 Add icons with no background for dropdown, needs to be pink and text to be vertically centred

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import IconNoBg from './IconNoBg';
 import { Link } from 'react-router-dom';
 
 const Menu = ({css, changeTopic, toggleMenu}) => {
@@ -10,49 +11,55 @@ const Menu = ({css, changeTopic, toggleMenu}) => {
         changeTopic('Home')
         return
       }}>
-        <li className="hot-pink" >Home</li>
+        <li className="hot-pink">Home</li>
       </Link>
       <Link to="/tax" style={{ textDecoration: 'none' }} onClick={() => {
         toggleMenu()
         changeTopic('Tax')
         return
       }}>
-        <li className="hot-pink">Tax</li>
+        <li className="hot-pink">
+          <IconNoBg topic="Tax" css="tax-no-bg" changeTopic={changeTopic}/>Tax</li>
       </Link>
       <Link to="/ownership" style={{ textDecoration: 'none' }} onClick={() => {
         toggleMenu()
         changeTopic('Ownership')
         return
       }}>
-        <li className="hot-pink">Ownership</li>
+        <li className="hot-pink">
+          <IconNoBg topic="Ownership" css="own-no-bg" changeTopic={changeTopic}/>Ownership</li>
       </Link>
       <Link to="/technology" style={{ textDecoration: 'none' }} onClick={() => {
         toggleMenu()
         changeTopic('Technology')
         return
       }}>
-        <li className="hot-pink">Technology</li>
+        <li className="hot-pink">
+          <IconNoBg topic="Technology" css="tech-no-bg" changeTopic={changeTopic}/>Technology</li>
       </Link>
       <Link to="/welfare" style={{ textDecoration: 'none' }} onClick={() => {
         toggleMenu()
         changeTopic('Welfare')
         return
       }}>
-        <li className="hot-pink">Welfare</li>
+        <li className="hot-pink">
+          <IconNoBg topic="Welfare" css="wel-no-bg" changeTopic={changeTopic}/>Welfare</li>
       </Link>
       <Link to="/workplace" style={{ textDecoration: 'none' }} onClick={() => {
         toggleMenu()
         changeTopic('Workplace')
         return
       }}>
-        <li className="hot-pink">Workplace</li>
+        <li className="hot-pink">
+          <IconNoBg topic="Workplace" css="work-no-bg" changeTopic={changeTopic}/>Workplace</li>
       </Link>
       <Link to="/askanon" style={{ textDecoration: 'none' }} onClick={() => {
         toggleMenu()
         changeTopic('Askanon')
         return
       }}>
-        <li className="hot-pink">Ask</li>
+        <li className="hot-pink">
+          <IconNoBg topic="Askanon" css="ask-no-bg" changeTopic={changeTopic}/>Ask</li>
       </Link>
     </ul>
   );
