95b8415fe04f6162568f6394e79eb1a31bab1418
Max Gerber <mgerber94@gmail.com>
2017-10-10 17:17:57 +0100

    Changed app background colour to light grey Relates #18

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
       currentPolicy: null,
       currentSnippet: null,
       menuDisplayed: false,
-      snippetVotes: [],
+      snippetVotes: []
     };
   }
 
@@ -31,29 +31,40 @@ class App extends Component {
   };
 
   updateSnippetVote = newVoteObj => {
-    this.setState({ snippetVotes: this.state.snippetVotes.concat(newVoteObj)});
+    this.setState({ snippetVotes: this.state.snippetVotes.concat(newVoteObj) });
   };
 
   render() {
     return (
       <Router>
-        <div className="mw6 center">
+        <div className="mw6 center app">
           <NavBar css="nav white relative h3" toggleMenu={this.toggleMenu} />
           <Menu id="menu" css={this.state.menuDisplayed ? 'fixed' : 'dn'} />
           <Switch>
-            <Route exact path="/" render={() => <Home changeTopic={this.changeTopic} />}
+            <Route
+              exact
+              path="/"
+              render={() => <Home changeTopic={this.changeTopic} />}
             />
             <Route
-              exact path="/ownership" render={() => (
+              exact
+              path="/ownership"
+              render={() => (
                 <PolicyCarousel
                   changeTopic={this.changeTopic}
                   toggleMenu={this.toggleMenu}
                   menuDisplayed={this.state.menuDisplayed}
                 />
-              )}/>
+              )}
+            />
             <Route exact path="/tax" render={() => <PolicyCarousel />} />
-            <Route exact path="/snippet" render={() => <SnippetView
-              updateSnippetVote={this.updateSnippetVote} />} />
+            <Route
+              exact
+              path="/snippet"
+              render={() => (
+                <SnippetView updateSnippetVote={this.updateSnippetVote} />
+              )}
+            />
             <Route exact path="/technology" render={() => <PolicyCarousel />} />
             <Route exact path="/welfare" render={() => <PolicyCarousel />} />
             <Route exact path="/workplace" render={() => <PolicyCarousel />} />
