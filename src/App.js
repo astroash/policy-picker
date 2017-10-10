9f5b6596eb8e704bd4e216576280027c70ab55c6
ameliejyc <amelie.chan@gmail.com>
2017-10-10 18:25:00 +0100

    #15 Get infograph structure on page with doughnut and line graph

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NavBar from './components/NavBar';
 import Menu from './components/Menu';
 import Modal from './components/Modal';
 import Form from './components/Form';
+import Infograph from './components/Infograph';
 
 class App extends Component {
   constructor() {
@@ -18,7 +19,7 @@ class App extends Component {
       currentPolicy: null,
       currentSnippet: null,
       menuDisplayed: false,
-      snippetVotes: [],
+      snippetVotes: []
     };
   }
 
@@ -31,7 +32,7 @@ class App extends Component {
   };
 
   updateSnippetVote = newVoteObj => {
-    this.setState({ snippetVotes: this.state.snippetVotes.concat(newVoteObj)});
+    this.setState({ snippetVotes: this.state.snippetVotes.concat(newVoteObj) });
   };
 
   render() {
@@ -41,23 +42,35 @@ class App extends Component {
           <NavBar css="nav white relative h3" toggleMenu={this.toggleMenu} />
           <Menu id="menu" css={this.state.menuDisplayed ? 'fixed' : 'dn'} />
           <Switch>
-            <Route exact path="/" render={() => <Home changeTopic={this.changeTopic} />}
+            <Route
+              exact
+              path="/"
+              render={() => <Home changeTopic={this.changeTopic} />}
             />
             <Route
-              exact path="/ownership" render={() => (
+              exact
+              path="/ownership"
+              render={() => (
                 <PolicyCarousel
                   changeTopic={this.changeTopic}
                   toggleMenu={this.toggleMenu}
                   menuDisplayed={this.state.menuDisplayed}
                 />
-              )}/>
+              )}
+            />
             <Route exact path="/tax" render={() => <PolicyCarousel />} />
-            <Route exact path="/snippet" render={() => <SnippetView
-              updateSnippetVote={this.updateSnippetVote} />} />
+            <Route
+              exact
+              path="/snippet"
+              render={() => (
+                <SnippetView updateSnippetVote={this.updateSnippetVote} />
+              )}
+            />
             <Route exact path="/technology" render={() => <PolicyCarousel />} />
             <Route exact path="/welfare" render={() => <PolicyCarousel />} />
             <Route exact path="/workplace" render={() => <PolicyCarousel />} />
             <Route exact path="/form" render={() => <Form />} />
+            <Route exact path="/infograph" render={() => <Infograph />} />
             <Route exact path="/summary" />
             {/* <Route exact path='/askanon' render={() => <Dummy />} /> */}
           </Switch>
