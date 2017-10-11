5b9c06b53aee0fc5d2e282abc52e09afc95a64ed
Max Gerber <mgerber94@gmail.com>
2017-10-11 12:58:51 +0100

    Changed icons to be nucleo Relates #19

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,57 +2,114 @@ import React from 'react';
 import IconNoBg from './IconNoBg';
 import { Link } from 'react-router-dom';
 
-const Menu = ({cssImg, changeTopic, toggleMenu}) => {
+const Menu = ({ cssImg, changeTopic, toggleMenu }) => {
   return (
     <ul
       className={`menu sans-serif list w-100 bg-white tc f2 mw6 hot-pink ${cssImg}`}>
-      <Link to="/" style={{ textDecoration: 'none' }} onClick={() => {
-        toggleMenu();
-        changeTopic('Home');
-      }}>
-        <li className="hot-pink">Home</li>
-      </Link>
-      <Link to="/tax" style={{ textDecoration: 'none' }} onClick={() => {
-        toggleMenu();
-        changeTopic('Tax');
-      }}>
+      <Link
+        to="/"
+        style={{ textDecoration: 'none' }}
+        onClick={() => {
+          toggleMenu();
+          changeTopic('Home');
+        }}>
         <li className="hot-pink">
-          <IconNoBg topic="Tax" cssImg="tax-no-bg" changeTopic={changeTopic}/>Tax</li>
+          <IconNoBg
+            topic="Home"
+            cssImg="home-no-bg"
+            changeTopic={changeTopic}
+          />Home
+        </li>
       </Link>
-      <Link to="/ownership" style={{ textDecoration: 'none' }} onClick={() => {
-        toggleMenu();
-        changeTopic('Ownership');
-      }}>
+      <Link
+        to="/tax"
+        style={{ textDecoration: 'none' }}
+        onClick={() => {
+          toggleMenu();
+          changeTopic('Tax');
+        }}>
         <li className="hot-pink">
-          <IconNoBg topic="Ownership" cssImg="own-no-bg" changeTopic={changeTopic}/>Ownership</li>
+          <IconNoBg
+            topic="Tax"
+            cssImg="tax-no-bg"
+            changeTopic={changeTopic}
+          />Tax
+        </li>
       </Link>
-      <Link to="/technology" style={{ textDecoration: 'none' }} onClick={() => {
-        toggleMenu();
-        changeTopic('Technology');
-      }}>
+      <Link
+        to="/ownership"
+        style={{ textDecoration: 'none' }}
+        onClick={() => {
+          toggleMenu();
+          changeTopic('Ownership');
+        }}>
         <li className="hot-pink">
-          <IconNoBg topic="Technology" cssImg="tech-no-bg" changeTopic={changeTopic}/>Technology</li>
+          <IconNoBg
+            topic="Ownership"
+            cssImg="own-no-bg"
+            changeTopic={changeTopic}
+          />Ownership
+        </li>
       </Link>
-      <Link to="/welfare" style={{ textDecoration: 'none' }} onClick={() => {
-        toggleMenu();
-        changeTopic('Welfare');
-      }}>
+      <Link
+        to="/technology"
+        style={{ textDecoration: 'none' }}
+        onClick={() => {
+          toggleMenu();
+          changeTopic('Technology');
+        }}>
         <li className="hot-pink">
-          <IconNoBg topic="Welfare" cssImg="wel-no-bg" changeTopic={changeTopic}/>Welfare</li>
+          <IconNoBg
+            topic="Technology"
+            cssImg="tech-no-bg"
+            changeTopic={changeTopic}
+          />Technology
+        </li>
       </Link>
-      <Link to="/workplace" style={{ textDecoration: 'none' }} onClick={() => {
-        toggleMenu();
-        changeTopic('Workplace');
-      }}>
+      <Link
+        to="/welfare"
+        style={{ textDecoration: 'none' }}
+        onClick={() => {
+          toggleMenu();
+          changeTopic('Welfare');
+        }}>
         <li className="hot-pink">
-          <IconNoBg topic="Workplace" cssImg="work-no-bg" changeTopic={changeTopic}/>Workplace</li>
+          <IconNoBg
+            topic="Welfare"
+            cssImg="wel-no-bg"
+            changeTopic={changeTopic}
+          />Welfare
+        </li>
       </Link>
-      <Link to="/askanon" style={{ textDecoration: 'none' }} onClick={() => {
-        toggleMenu();
-        changeTopic('Askanon');
-      }}>
+      <Link
+        to="/workplace"
+        style={{ textDecoration: 'none' }}
+        onClick={() => {
+          toggleMenu();
+          changeTopic('Workplace');
+        }}>
         <li className="hot-pink">
-          <IconNoBg topic="Askanon" cssImg="ask-no-bg" changeTopic={changeTopic}/>Ask</li>
+          <IconNoBg
+            topic="Workplace"
+            cssImg="work-no-bg"
+            changeTopic={changeTopic}
+          />Workplace
+        </li>
+      </Link>
+      <Link
+        to="/askanon"
+        style={{ textDecoration: 'none' }}
+        onClick={() => {
+          toggleMenu();
+          changeTopic('Askanon');
+        }}>
+        <li className="hot-pink">
+          <IconNoBg
+            topic="Askanon"
+            cssImg="ask-no-bg"
+            changeTopic={changeTopic}
+          />Ask
+        </li>
       </Link>
     </ul>
   );
