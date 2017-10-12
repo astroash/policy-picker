991f78f04b148b9a6098ab284a0f7460bb77a6f7
astroash <astroaisha@gmail.com>
2017-10-12 12:47:15 +0100

    #17 Add Max's magic

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -2,17 +2,14 @@ import React from 'react';
 import IconSnippet from './IconSnippet';
 import policies from '../data/policies.json';
 
-const getSnippetTitle = (voteId) => {
-  const policyKeys = voteId.split('.');
-  const topic = policyKeys[0];
-  const policy = policyKeys[1];
-  const snippet = policyKeys[2];
-  return policies[topic][policy].snippets[snippet]["mini-desc"];
+const getSnippetTitle = (json, voteId) => {
+  const [topic, policy, snippet ] = voteId.split('.');
+  return json[topic][policy].snippets[snippet]["mini-desc"];
 }
 
 const Summary= ({snippetUpvotes, snippetDownvotes}) => {
-  const upVotesList =  snippetUpvotes.map(voteId => <li>{getSnippetTitle(voteId)}</li>)
-  const downVotesList =  snippetDownvotes.map(voteId => <li>{getSnippetTitle(voteId)}</li>)
+  const upVotesList =  snippetUpvotes.map(voteId => <li>{getSnippetTitle(policies, voteId)}</li>)
+  const downVotesList =  snippetDownvotes.map(voteId => <li>{getSnippetTitle(policies, voteId)}</li>)
   return (
     <div className="mh3">
       <h1 className="tc ma0 mb3">Summary</h1>
