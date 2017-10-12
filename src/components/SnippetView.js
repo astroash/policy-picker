12b4c14a89f93c2c1cc8776b875ffcd66849bb23
astroash <astroaisha@gmail.com>
2017-10-12 11:37:05 +0100

    #17 remove console.logs oops :hankey:

--- src/components/SnippetView.js
+++ src/components/SnippetView.js
@@ -53,8 +53,6 @@ class SnippetView extends Component {
           ref="stack"
           throwout={e => {
             e.target.remove();
-            // let voteObj = {};
-            // voteObj[e.target.firstChild.id] = voteTypeOf(e.throwDirection);
             this.props.updateSnippetVote(e.target.firstChild.id, voteTypeOf(e.throwDirection));
           }}>
           {this.arrayOfSnippetComponents(policies.EC[this.props.currentPolicy].snippets)}
