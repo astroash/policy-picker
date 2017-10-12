4bc5d0f8f027f46d5c721374c1c9932ec24f34ec
ameliejyc <amelie.chan@gmail.com>
2017-10-12 17:43:30 +0100

    #19 Add in modal screenshots and adjust spacing

--- src/components/ModalCard.js
+++ src/components/ModalCard.js
@@ -4,9 +4,9 @@ const ModalCard = ({ topinfo, bottominfo, imgsrc, ...props }) => {
   return (
     <div className="modal-card" {...props}>
       <div>
-        <h1 className="card-title f3">{topinfo}</h1>
+        <h1 className="f3 pa3">{topinfo}</h1>
         <img src={imgsrc} alt={topinfo} className="card-img" />
-        <p className="card-desc">{bottominfo}</p>
+        <p className="card-desc mt4">{bottominfo}</p>
       </div>
     </div>
   );
