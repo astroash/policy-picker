3db046692cb05043131f30d74745f7453ee572a3
ameliejyc <amelie.chan@gmail.com>
2017-10-12 12:54:08 +0100

    #43 Loading progress

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import CircularProgressbar from 'react-circular-progressbar';
 import '../css/Loading.css';
+import CircularProgressbar from 'react-circular-progressbar';
 
 class ChangingProgressbar extends React.Component {
   constructor(props) {
@@ -11,10 +11,13 @@ class ChangingProgressbar extends React.Component {
   }
 
   componentDidMount() {
-    setInterval(() => {
+    let refreshInterval = setInterval(() => {
       this.setState({
-        currentPercentageIndex: (this.state.currentPercentageIndex + 1) % this.props.percentages.length
+        currentPercentageIndex: (this.state.currentPercentageIndex + 1) % (this.props.percentages.length)
       });
+      if (this.props.currentPercentageIndex === 99) {
+        clearInterval(refreshInterval);
+      }
     }, this.props.interval);
   }
 
@@ -22,19 +25,27 @@ class ChangingProgressbar extends React.Component {
     return <CircularProgressbar {...this.props} percentage={this.props.percentages[this.state.currentPercentageIndex]} />;
   }
 }
+
 ChangingProgressbar.defaultProps = {
-  interval: 40,
-  strokeWidth: 4
+  interval: 200,
+  stroke: 3
 }
 
-const Loading = () => {
-  return (
-    <div className="loading mt5">
-      <ChangingProgressbar
-        percentages={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]}
-      />
-    </div>
-  );
-};
+class Loading extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row mt-3">
+          <ChangingProgressbar
+            percentages={[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 99]}
+            classForPercentage={(percentage) => {
+              return percentage === 99 ? 'complete' : 'incomplete';
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Loading;
